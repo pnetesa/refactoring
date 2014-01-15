0ba69b045024b149542445c03912fcfab3a78540
pnetesa <pnetesa@enova.com>
2014-01-15 13:30:00 +0200

    Add project files.

---
+++ app/src/main/java/com/examples/Program.java
@@ -0,0 +1,11 @@
+package com.examples;
+
+//import static com.examples.util.Print.*;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
---
+++ app/src/main/java/com/examples/_01_ExtractMethod.java
@@ -0,0 +1,147 @@
+package com.examples;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.examples.util.Print.print;
+
+public class _01_ExtractMethod {
+
+    public static void main(String[] args) {
+
+        Before before = new Before();
+        before.printOwing(1234.56);
+
+        print();
+
+        After after = new After();
+        after.printOwing(1234.56);
+
+        print();
+
+        Before2 before2 = new Before2();
+        before2.printOwing();
+
+        print();
+
+        After2 after2 = new After2();
+        after2.printOwing();
+        after2.printOwing(1234.56);
+	}
+}
+
+class Before {
+
+    private String mName = "Before";
+
+    private void printBanner() {
+    }
+
+    public void printOwing(double amount) {
+        printBanner();
+
+        // Print details
+        print("name: " + mName);
+        print("amount: " + amount);
+    }
+}
+
+class After {
+
+    private String mName = "After";
+
+    private void printBanner() {
+    }
+
+    public void printOwing(double amount) {
+        printBanner();
+        printDetails(amount);
+    }
+
+    private void printDetails(double amount) {
+        print("name: " + mName);
+        print("amount: " + amount);
+    }
+}
+
+class Before2 {
+
+    private String mName = "Before";
+    private List<Order> mOrders = new ArrayList<Order>(
+            Arrays.asList(new Order(1234.56), new Order(65.1234), new Order(987.654)));
+
+    public void printOwing() {
+
+        double outstanding = 0;
+
+        // Print banner
+        print ("**************************");
+        print ("***** Customer Owes ******");
+        print ("**************************");
+
+        // Calculate outstanding
+        for (Order order : mOrders) {
+            outstanding += order.getAmount();
+        }
+
+        // Print details
+        print("name: " + mName);
+        print("amount: " + outstanding);
+    }
+}
+
+class After2 {
+
+    private String mName = "After";
+    private List<Order> mOrders = new ArrayList<Order>(
+            Arrays.asList(new Order(1234.56), new Order(65.1234), new Order(987.654)));
+
+    public void printOwing() {
+        printBanner();
+        double outstanding = getOutstanding(0);
+        printDetails(outstanding);
+    }
+
+    public void printOwing(double previousAmount) {
+
+        double outstanding = previousAmount * 1.2;
+        printBanner();
+        outstanding = getOutstanding(outstanding);
+        printDetails(outstanding);
+    }
+
+    private double getOutstanding(double initialValue) {
+
+        double result = initialValue;
+
+        for (Order order : mOrders) {
+            result += order.getAmount();
+        }
+        return result;
+    }
+
+    private void printDetails(double outstanding) {
+        print("name: " + mName);
+        print("amount: " + outstanding);
+    }
+
+    private void printBanner() {
+        print ("**************************");
+        print ("***** Customer Owes ******");
+        print ("**************************");
+    }
+}
+
+class Order {
+
+    private double mAmount;
+
+    public Order(double amount) {
+        mAmount = amount;
+    }
+
+    public double getAmount() {
+        return mAmount;
+    }
+}
